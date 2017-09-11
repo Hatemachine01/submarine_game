b2e7cbae725d6e5989b557b6e3bcf43d5bbe8fb5
Julio <juliodevasquez@gmail.com>
2017-09-10 20:27:15 -0500

    added loging system

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,31 @@
 class SessionsController < ApplicationController
+
+
+def new
+end
+
+
+def create
+ @user = User.find_by_email(user_params[:email])
+# If the user exists AND the password entered is correct.
+	if @user && @user.authenticate(user_params[:password])
+# Save the user id inside the browser cookie. This is how we keep the user 
+# logged in when they navigate around our website.
+   log_in @user
+   redirect_to user_path(current_user)
+    else
+    # If user's login doesn't work, send them back to the login form.
+      redirect_to '/login'
+    end
+  end
+
+
+
+private
+
+
+def user_params
+      params.require(:session).permit(:name, :email, :password)
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,16 @@ Rails.application.routes.draw do
 resources :users 
 
 
+
+
+
+get    '/login',   to: 'sessions#new'
+
+
+
+
+post   '/login',   to: 'sessions#create'
+
 root "application#index"
 
 
